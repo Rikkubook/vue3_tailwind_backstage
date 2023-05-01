e559548959a350b126eb8a37e56abf614f7168c4
Rikku <rikkubook@gmail.com>
2023-05-01 22:30:44 +0800

    feat(3-7 Notification Function): 抽 function 出來使用

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -19,21 +19,24 @@
 </template>
 
 <script>
-import { useConfirmModel } from '@/composables/useConfirmModal'
+import { notify,  successNotify, warningNotify, errorNotify} from '@/composables/useNotification'
 import {ref, computed} from 'vue'
-import { notify } from "@kyvg/vue3-notification";
+// import { notify } from "@kyvg/vue3-notification";
 
 
 export default {
   setup(){
-    const { showModal, confirmTitle } =  useConfirmModel()
     const open = ()=>{
-      notify({
-        // type: 'success',
-        title: "更新成功",
-        text: "已經更新成功了",
-        duration: 10000000
-    });
+      notify('更新成功')
+      successNotify('更新成功')
+      warningNotify('警示訊息')
+      errorNotify('錯誤訊息','sdasd',{duration: 10000000})
+    //   notify({
+    //     // type: 'success',
+    //     title: "更新成功",
+    //     text: "已經更新成功了",
+    //     duration: 10000000
+    // });
     }
 
     return{ open }
